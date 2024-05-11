0ab3f13ca1f2e406ac50d9d52b72e74f0af49b8c
Sylwia Wiktorska <sylwia.wiktorska27@gmail.com>
2024-05-11 12:17:10 +0200

    Hotjar and cypress

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,33 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if each slide description is displayed correctly', function () {
+    cy.visit('http://localhost:3000');
+
+    cy.get('.swiper-slide').each(($slide, index) => {
+      const title = $slide.find('h1').text().trim();
+      const description = $slide.find('p').text().trim();
+
+      cy.wrap(title).should('not.be.empty');
+      cy.wrap(description).should('not.be.empty');
+
+      switch(index) {
+        case 0:
+          cy.wrap(title).should('eq', 'Rome');
+          cy.wrap(description).should('eq', 'Italy');
+          break;
+        case 1:
+          cy.wrap(title).should('eq', 'London');
+          cy.wrap(description).should('eq', 'United Kingdom');
+          break;
+        case 2:
+          cy.wrap(title).should('eq', 'Paris');
+          cy.wrap(description).should('eq', 'France');
+          break;
+      }
+    });
+  });
+});
+
